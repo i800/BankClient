8b205b08e622f329c4738f04056ed7994d76711d
i800 <usatschoff@gmail.com>
2017-11-26 03:32:02 +0200

    MainWindow look complete (without logic).

--- GUI/MainWindow.cpp
+++ GUI/MainWindow.cpp
@@ -7,8 +7,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowFlags(Qt::FramelessWindowHint
-                       | Qt::WindowStaysOnBottomHint);
+    this->setWindowFlags(Qt::WindowStaysOnBottomHint
+                       | Qt::MSWindowsFixedSizeDialogHint);
     this->setMinimumSize(size());
     this->setMaximumSize(size());
 }

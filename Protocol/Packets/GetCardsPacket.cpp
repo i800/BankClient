778ac89b92cda22fb87a8463efbaac058a258395
i800 <usatschoff@gmail.com>
2017-12-01 22:14:54 +0200

    GetCardsPacket modified.

--- Protocol/Packets/GetCardsPacket.cpp
+++ Protocol/Packets/GetCardsPacket.cpp
@@ -2,12 +2,15 @@
 
 GetCardsPacket::GetCardsPacket():
     _token(0),
-    _userId(0)
+    _userId(0),
+    _terminalId(0)
 {}
 
-GetCardsPacket::GetCardsPacket(const quint64 token, const quint64 userId):
+GetCardsPacket::GetCardsPacket(const quint64 token, const quint64 userId,
+                               const quint32 terminalId):
     _token(token),
-    _userId(userId)
+    _userId(userId),
+    _terminalId(terminalId)
 {}
 
 GetCardsPacket::~GetCardsPacket()
@@ -28,6 +31,7 @@ QByteArray GetCardsPacket::specificDump() const
     QByteArray data;
     data.append((char*)&_token, sizeof(_token));
     data.append((char*)&_userId, sizeof(_userId));
+    data.append((char*)&_terminalId, sizeof(_terminalId));
     return data;
 }
 
@@ -35,4 +39,5 @@ void GetCardsPacket::specificLoad(QBuffer& data)
 {
     data.read((char*)&_token, sizeof(_token));
     data.read((char*)&_userId, sizeof(_userId));
+    data.read((char*)&_terminalId, sizeof(_terminalId));
 }
